8e3ff30e9977bdd8b59bf949fe8d41301ab6f31e
David Crook <idcrook@users.noreply.github.com>
2020-03-04 17:43:14 -0700

    finish 21_shader_ubo

--- code/21_descriptor_layout.cpp
+++ code/21_descriptor_layout.cpp
@@ -545,7 +545,7 @@ private:
         uboLayoutBinding.descriptorCount = 1;
         uboLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
         uboLayoutBinding.pImmutableSamplers = nullptr;
-        uboLayoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+        uboLayoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT; // could be flags set or VK_SHADER_STAGE_ALL_GRAPHICS
 
         VkDescriptorSetLayoutCreateInfo layoutInfo = {};
         layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
@@ -917,6 +917,7 @@ private:
         ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
         ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
         ubo.proj = glm::perspective(glm::radians(45.0f), swapChainExtent.width / (float) swapChainExtent.height, 0.1f, 10.0f);
+        // GLM from OpenGL: the y coordinate is inverted; invert it back to match our vulkan y basis
         ubo.proj[1][1] *= -1;
 
         void* data;
